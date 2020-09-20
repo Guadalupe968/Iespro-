60aca8c3e37e51c0581b01db8c42ed3e29f34833
Guadalupe968 <guadalupe.morales@unidech.mx>
2020-09-20 14:20:13 -0500

    modelo de datos terminado

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
+from flask import Flask
 
-@app.route("/")
-def home_page():
-  return render_template("home.html")
+import views
+from database import Database
+from movie import Movie
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings")
+
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/movies", view_func=views.movies_page)
+    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+
+    db = Database()
+    db.add_movie(Movie("Slaughterhouse-Five", year=1972))
+    db.add_movie(Movie("The Shining"))
+    app.config["db"] = db
+
+    return app
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080, debug=True)
+    app = create_app()
+    port = app.config.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
+
